3756e180b7568b6233c6fda6248d9bf42fa95541
imtripleq <flamework89@hotmail.com>
2022-02-01 09:46:04 +1100

    done

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,8 @@
 class Person
-  #your code here
+  def initialize(attributes)
+    attributes.each do |key, value|
+      self.class.attr_accessor(key)
+      self.send((key.to_s + '='), value)
+    end
+  end
 end
